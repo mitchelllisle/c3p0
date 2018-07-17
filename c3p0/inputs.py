10ca14de9124a2f2df4ba2dd59194fd87f4a28ff
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2018-07-17 14:05:30 +1000

    updated profule

--- c3p0/inputs.py
+++ c3p0/inputs.py
@@ -1,51 +1,65 @@
 import json
 import os
 
-class pyprofile:
+class profile:
     '''
-    Pyprofile is a way to manage keys, secrets and other information that you want
+    env = profile()
+    env.read()
+    env.add({"hello" : {"test" : "tetOne"}})
+    env.delete("hello")
+    env.profile
+    
+    profile is a way to manage keys, secrets and other information that you want
     to store for your Python environment. Reason for this came from no real alternative
-    that works in a similar way to .Rprofile.
+    that works in a similar way to the way R works with its .Rprofile.
+
     Basics of this class are:
     create: Add a new profile. All profiles are json documents
     add: Add a key value pair, or nested object to your profile
     delete: Remove key from profile
     read: Read profile
+
+    If you have a profile in your HOME directory it will default to using that. Note: Mac is the only OS
+    supported for this.
     '''
-    def create(config = {}, location = None):
+    def __init__(self):
+        self.profile = {}
+
+    def create(self, config = {}, location = None):
         if location == None:
             location = str(os.getenv("HOME")) + "/pyprofile.json"
         assert type(config) == dict
         if os.path.isfile(location):
             raise Exception(location + " is already a valid profile. Please choose another location or use `add` to add to existing profile.")
 
-        pyprofile = {}
-        pyprofile.update(config)
+        self.profile.update(config)
         with open(location, 'w') as outfile:
-            json.dump(pyprofile, outfile)
+            json.dump(self.profile, outfile)
         return location
 
-    def read(location = None):
+    def read(self, location = None):
         if location == None:
             location = str(os.getenv("HOME")) + "/pyprofile.json"
-        text =  open(location).read()
-        profile = json.loads(text)
-        return profile
+        text = open(location).read()
+        config = json.loads(text)
+        self.profile.update(config)
 
-    def add(config, location = None):
+    def add(self, config, location = None):
         if location == None:
             location = str(os.getenv("HOME")) + "/pyprofile.json"
-        profile = pyprofile.read(location)
-        profile.update(config)
+        text = open(location).read()
+        oldconfig = json.loads(text)
+        self.profile.update(oldconfig)
+        self.profile.update(config)
         with open(location, 'w') as outfile:
-            json.dump(profile, outfile)
-        return profile
+            json.dump(self.profile, outfile)
 
-    def delete(config, location = None):
+    def delete(self, config, location = None):
         if location == None:
             location = str(os.getenv("HOME")) + "/pyprofile.json"
-        profile = pyprofile.read(location)
-        profile.pop(config)
+        text = open(location).read()
+        oldconfig = json.loads(text)
+        self.profile.update(oldconfig)
+        self.profile.pop(config)
         with open(location, 'w') as outfile:
-            json.dump(profile, outfile)
-        return profile
+            json.dump(self.profile, outfile)
