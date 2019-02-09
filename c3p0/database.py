105edd016f80b2caa90337f477d693081d06474a
Mitchell Lisle <lislemitchell@gmail.com>
2019-02-09 11:07:18 +1100

    fix: cleanup

--- c3p0/database.py
+++ c3p0/database.py
@@ -1,11 +1,10 @@
 import psycopg2 as ps
 import pandas as pd
-import numpy
 import martha as mh
 import boto3
-import json
 from .aws import extractBucketName
 from .aws import fetchS3
+
 
 def queryPostgres(host, port, user, password, database, query):
     '''
@@ -37,11 +36,12 @@ def queryPostgres(host, port, user, password, database, query):
             pass
 
         rows = cur.fetchall()
-        data = pd.DataFrame(rows, columns = [columns])
+        data = pd.DataFrame(rows, columns=[columns])
         conn.close
         return data
     except Exception as e:
-         raise Exception(str(e))
+        raise Exception(str(e))
+
 
 def createFieldReplacement(repeats):
         repeats = repeats - 1
@@ -51,22 +51,15 @@ def createFieldReplacement(repeats):
         fieldReplacement = "(" + fieldReplacement + ")"
         return fieldReplacement
 
-data = {
-    "jsontest" : [{
-        "hello" : "inserttest"
-    }]
-}
 
-env = c3p0.source()
-host, port, username, password, database, table, data, columns, upsertPrimaryKey
-def insertToPostgres(host, port, username, password, database, table, data, columns, upsertPrimaryKey = None):
+def insertToPostgres(host, port, username, password, database, table, data, columns, upsertPrimaryKey=None):
     try:
         # data = data.where((pd.notnull(data)), None)
         rowsToInsert = len(data)
         fieldReplacement = createFieldReplacement(len(data.keys()))
-        conn = ps.connect("dbname='" + database + "' user='" + username + "' host='" + host + "' port='" + port + "' password='" +  password + "'")
+        conn = ps.connect("dbname='" + database + "' user='" + username + "' host='" + host + "' port='" + port + "' password='" + password + "'")
         cur = conn.cursor()
-        allRowSql = bytes(b"INSERT INTO " + table.encode() + b" (" + mh.cleanUpString(str(data.columns.values.tolist()), ["[", "]", "'"], {"'" : ""}).encode() + b") VALUES ")
+        allRowSql = bytes(b"INSERT INTO " + table.encode() + b" (" + mh.cleanUpString(str(data.columns.values.tolist()), ["[", "]", "'"], {"'": ""}).encode() + b") VALUES ")
 
         for i in range(rowsToInsert):
             row = data.iloc[i].values.tolist()
@@ -77,7 +70,7 @@ def insertToPostgres(host, port, username, password, database, table, data, colu
 
             allRowSql = allRowSql + rowSql
 
-        if upsertPrimaryKey != None:
+        if upsertPrimaryKey is not None:
             upsertPrimaryKey = str(upsertPrimaryKey).replace("[", "").replace("]", "").replace("'", "")
 
             baseUpsert = b" ON CONFLICT (" + upsertPrimaryKey.encode() + b") DO UPDATE SET "
@@ -95,54 +88,23 @@ def insertToPostgres(host, port, username, password, database, table, data, colu
         cur.execute(allRowSql)
         conn.commit()
         conn.close()
-        results = {"columns" : len(data.columns), "rows" : len(data)}
+        results = {"columns": len(data.columns), "rows": len(data)}
         return results
     except Exception as e:
         conn.close()
         raise Exception(str(e))
 
-def typeConvert(column, values, types):
-    type = types[column]
-    if type == "json":
-        values = {k: json.dumps(v) for k, v in values.items()}
-    elif type == "text":
-        values = {k: str(v) for k, v in values.items()}
-    elif type == "int":
-        values = {k: int(v) for k, v in values.items()}
-    return values
-
-def putPostgres(host, port, username, password, database, table, data, types):
-    value_list = list()
-    column_list = list()
-
-    for column, value in data.items():
-        column_list.append(sql.Identifier(column))
-        value = typeConvert(column, value, types)
-        value_list.append(value)
-
-    insert_query = sql.SQL("INSERT INTO {} ({}) VALUES ({}) ON CONFLICT DO NOTHING").format(
-                    sql.Identifier(table),
-                    sql.SQL(', ').join(column_list),
-                    sql.SQL(', ').join([sql.Placeholder()] * len(value_list)))
-
-    conn = ps.connect("dbname='" + database + "' user='" + username + "' host='" + host + "' port='" + str(port) + "' password='" +  password + "'")
-
-    with conn.cursor() as p_cursor:
-        p_cursor.executemany(insert_query, tuple(value_list))
-
-    conn.commit()
-    conn.close()
-    pass
 
 def getExecutionStatus(executionId, client):
-    execution = client.get_query_execution(QueryExecutionId = executionId)
+    execution = client.get_query_execution(QueryExecutionId=executionId)
     outputLocation = execution['QueryExecution']['ResultConfiguration']['OutputLocation']
     status = execution['QueryExecution']['Status']['State']
     return status, outputLocation
 
+
 def queryAthena(access_key, access_secret, query, resultLocation):
-    client = boto3.client('athena', aws_access_key_id = access_key, aws_secret_access_key = access_secret)
-    queryRequest = client.start_query_execution(QueryString = query, ResultConfiguration = {'OutputLocation' : resultLocation})
+    client = boto3.client('athena', aws_access_key_id=access_key, aws_secret_access_key=access_secret)
+    queryRequest = client.start_query_execution(QueryString=query, ResultConfiguration={'OutputLocation': resultLocation})
 
     executionStatus = getExecutionStatus(str(queryRequest['QueryExecutionId']), client)
     status = executionStatus[0]
