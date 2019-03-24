926f679ea27d9eb1f563d5577071997cbc1697e6
Mitchell Lisle <lislemitchell@gmail.com>
2019-03-24 16:08:05 +1100

    cleanup

--- c3p0/inputs.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-def source(location = None):
-    if location == None:
-        location = str(os.getenv("HOME")) + "/.pyprofile"
-    text = open(location).read()
-    config = json.loads(text)
-    return config
