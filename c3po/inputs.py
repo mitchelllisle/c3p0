dbc12a6dbd3ace6c67019984b1adc054003ed5a9
Mitchell Lisle <lislemitchell@gmail.com>
2019-06-17 18:58:47 +0100

    rename of Secrets

--- c3po/inputs.py
+++ c3po/inputs.py
@@ -4,24 +4,21 @@ from .inputs_errors import InputError
 
 
 class Secrets:
-    def __init__(self, name, location=None):
-        self.name = name
-
+    def __init__(self, location=None):
         if location is None:
             location = str(os.getenv("HOME")) + "/.pyprofile"
         text = open(location).read()
         self.config = json.loads(text)
 
+    def load(self, name):
+        self.name = name
         if name in self.config.keys():
             self.loaded_credential = self.config.get(name)
         else:
             raise InputError.NoSecret("Secret name {} doesn't exist".format(self.name))
 
-    @property
-    def credentials(self):
         return self.loaded_credential
 
-    @property
     def names(self):
         return list(self.config.keys())
 
