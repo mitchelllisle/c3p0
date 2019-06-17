8667c4f4c9abdf7ad1ac711b19cedb0c9443d145
Mitchell Lisle <lislemitchell@gmail.com>
2019-06-17 10:15:49 +0100

    bump release

--- setup.py
+++ setup.py
@@ -4,15 +4,13 @@
 :copyright: (c) 2017 by Mitchell Lisle
 :license: MIT, see LICENSE for more details.
 """
-import os
-import sys
 
 from setuptools import setup
 from setuptools import find_packages
-from setuptools.command.install import install
+
 
 setup(name='c3po',
-      version='SomeGiraffe',
+      version='OctastyNopus',
       description='A python package for working with various services',
       url='http://github.com/mitchelllisle/c3po',
       author='Mitchell Lisle',
