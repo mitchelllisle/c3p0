32880b441562e8029efd4039e94461cb9261e23b
Mitchell Lisle <lislemitchell@gmail.com>
2019-06-17 18:29:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='c3po',
       url='http://github.com/mitchelllisle/c3po',
       author='Mitchell Lisle',
       author_email='m.lisle90@gmail.com',
-      packages=['c3po', 'c3po.aws'],
+      packages=['c3po'],
       license='MIT',
       install_requires=[
           'boto3',
