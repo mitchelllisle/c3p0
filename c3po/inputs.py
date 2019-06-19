4f2402db52e20aee4f65ec61b3df68bbfc4b8738
Mitchell Lisle <lislemitchell@gmail.com>
2019-06-19 19:54:48 +0100

    Cosmetic updates

--- c3po/inputs.py
+++ c3po/inputs.py
@@ -27,6 +27,7 @@ class Secrets:
 def source(location=None):
     if location is None:
         location = str(os.getenv("HOME")) + "/.pyprofile"
+
     text = open(location).read()
     config = json.loads(text)
     return config
